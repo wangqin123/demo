2623d65007d548b762de184d6236abec20e7edc3
wangqin <wangqin@DESKTOP-PJVERKF>
2016-08-01 11:56:36 +0800

    添加bean

--- src/main/java/demo/demo.java
+++ src/main/java/demo/demo.java
@@ -1,5 +1,42 @@
 package demo;
 
-public class demo {
+import java.io.Serializable;
+
+public class demo  implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+
+	private String mobile;
+
+	private Integer sex;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public Integer getSex() {
+		return sex;
+	}
+
+	public void setSex(Integer sex) {
+		this.sex = sex;
+	}
+	
 
 }
